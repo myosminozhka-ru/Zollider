399a6d81cf978a7ac153d1804fa493b2fd679941
arsosmi <sheraliev@myosminozhka.ru>
2023-05-30 18:40:23 +0600

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  server: {
+    host: "0.0.0.0", // default: localhost // (0.0.0.0 for open on mob) // 127.0.0.1 for prod
+    port: 3021,
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Zollider',
+    htmlAttrs: {
+      lang: 'ru'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Zollider' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' },
+    ],
+    script: [
+      { src: '/wow.js', defer: true },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "@/assets/css/swiper-bundle.min.css",
+    "@/assets/css/animate.min.css",
+    "@/assets/scss/_fonts.scss",
+    "@/assets/scss/_normalize.scss",
+    "@/assets/scss/_settings.scss",
+  ],
+
+  styleResources: {
+    scss: ["./assets/scss/_variables.scss"],
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    // { src: "@/plugins/smooth.js", ssr: false,}
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    "@nuxtjs/style-resources",
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    // fix to work with swiperjs 8 - need to run with standalone:true. That can make some troubles.
+    standalone: true,
+    extend(config, ctx) {
+      // fix to work with swiperjs 8 add needed deps. you can get them from error when doing nuxt generate
+      config.externals = [
+        {
+          encoding: "encoding",
+        },
+      ];
+    },
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="IndexPage">
+    <h1>Hello, world!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
