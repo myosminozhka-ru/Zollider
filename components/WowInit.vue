56866c914a1709b3ff050d5f21f0a50530771171
sheraliev@myosminozhka.ru <sheraliev@myosminozhka.ru>
2023-06-22 20:43:58 +0600

    fix

--- components/WowInit.vue
+++ components/WowInit.vue
@@ -5,13 +5,6 @@ export default {
   mounted() {
     if (process.client) {
       new WOW().init();
-      const inputs = document.querySelectorAll('input[type="tel"]')
-      const instances = []
-      inputs.forEach(el => {
-        instances.push(
-          IMask(el, {mask: '+{7 }000-000-00-00'})
-        )
-      })
     }
   },
 }
